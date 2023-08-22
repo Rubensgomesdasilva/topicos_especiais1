6048aa6d762a4bae31d05abc44fe645b953b2b08
Rubensgomesdasilva <104952992+Rubensgomesdasilva@users.noreply.github.com>
2023-08-21 21:27:22 -0400

    exercicio

---
+++ exercicio1.js
@@ -0,0 +1,14 @@
+function calcular(salario)
+{
+
+let salario;
+let nome;
+let reajuste;
+let valor;
+
+nome ="Rubens";
+salario ="'280.00";
+reajuste = salario +(salario * 0.20);
+valor = 280 + reajuste;
+console.log(nome +'\n'+ salario+'\n'+ reajuste+'\n'+ valor)
+
